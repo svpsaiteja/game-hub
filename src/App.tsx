f7d7beceff451ea6a6b8dc1bab65bda631b362e3
Saiteja S <saiteja.sami@biofourmis.com>
2024-07-23 23:28:01 +0530

    Displaying genres

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,16 @@ function App() {
         base: `"nav" "main"`,
         lg: `"nav nav" "aside main"`,
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "250px 1fr",
+      }}
     >
       <GridItem area="nav">
         <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem paddingX={5} area="aside">
           <GenreList />
         </GridItem>
       </Show>
