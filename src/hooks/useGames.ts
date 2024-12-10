5b04c49f1d1d99fd6415e5b45baf571916f3943e
Saiteja <53438101+svpsaiteja@users.noreply.github.com>
2024-12-10 13:38:49 +0530

    Refactored entity interfaces

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,7 +2,7 @@ import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import APIClient, { FetchResponse } from "../services/api-client";
 import ms from "ms";
 import useGameQueryStore, { GameQuery } from "../store";
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
 
